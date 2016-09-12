2e11b9016458e700dd5367a2d695c463c6ce4cb1
Bradley Bossard <bradleybossard@gmail.com>
2016-09-12 04:29:28 +0000

    Initial Angular call to get data

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('point1');
+
+var app = angular.module('myApp', [])
+.controller('myCtrl', function($scope, $http) {
+	$scope.getData = function() {
+		$http.get('props.json').then(
+				function (res) {
+						// TODO: Do stuff here
+				},
+				function () {
+					console.log("Error fetching json");
+				});
+  }
+
+  $scope.init = function() {
+    $scope.getData();
+  }
+
+  $scope.init();
+});
